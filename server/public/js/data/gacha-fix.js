c904e21dbdbbe19b90a6ec2d6b4b145faac961eb
thanawutmalirum29-creator <thanawutmalirum29@gmail.com>
2026-06-30 15:04:44 +0700

    Add files via upload

---
+++ server/public/js/data/gacha-fix.js
@@ -0,0 +1,348 @@
+function enforceMinCost(poolKey, cost) {
+  switch (poolKey) {
+    case "GACHA10011": // ตู้1
+     if (
+    cost <= 0  || cost <= 1  || cost <= 2  || cost <= 3  || cost <= 4  ||
+    cost <= 5  || cost <= 6  || cost <= 7  || cost <= 8  || cost <= 9  ||
+    cost <= 10 || cost <= 11 || cost <= 12 || cost <= 13 || cost <= 14 ||
+    cost <= 15 || cost <= 16 || cost <= 17 || cost <= 18 || cost <= 19 ||
+    cost <= 20 || cost <= 21 || cost <= 22 || cost <= 23 || cost <= 24 ||
+    cost <= 25 || cost <= 26 || cost <= 27 || cost <= 28 || cost <= 29 ||
+    cost <= 30 || cost <= 31 || cost <= 32 || cost <= 33 || cost <= 34 ||
+    cost <= 35 || cost <= 36 || cost <= 37 || cost <= 38 || cost <= 39 ||
+    cost <= 40 || cost <= 41 || cost <= 42 || cost <= 43 || cost <= 44 ||
+    cost <= 45 || cost <= 46 || cost <= 47 || cost <= 48 || cost <= 49 ||
+    cost <= 50 || cost <= 51 || cost <= 52 || cost <= 53 || cost <= 54 ||
+    cost <= 55 || cost <= 56 || cost <= 57 || cost <= 58 || cost <= 59 ||
+    cost <= 60 || cost <= 61 || cost <= 62 || cost <= 63 || cost <= 64 ||
+    cost <= 65 || cost <= 66 || cost <= 67 || cost <= 68 || cost <= 69 ||
+    cost <= 70 || cost <= 71 || cost <= 72 || cost <= 73 || cost <= 74 ||
+    cost <= 75 || cost <= 76 || cost <= 77 || cost <= 78 || cost <= 79 ||
+    cost <= 80 || cost <= 81 || cost <= 82 || cost <= 83 || cost <= 84 ||
+    cost <= 85 || cost <= 86 || cost <= 87 || cost <= 88 || cost <= 89 ||
+    cost <= 90 || cost <= 91 || cost <= 92 || cost <= 93 || cost <= 94 ||
+    cost <= 95 || cost <= 96 || cost <= 97 || cost <= 98 || cost <= 99 ||
+    cost >= 101 || cost >= 102 || cost >= 103 || cost >= 104 || cost >= 105
+) {
+        cost = 999999999999999992671;
+      }
+      break;
+
+    case "GACHA10012": // ตู้2
+      if (
+        cost <= 0  || cost <= 1  || cost <= 2  || cost <= 3  || cost <= 4  ||
+    cost <= 5  || cost <= 6  || cost <= 7  || cost <= 8  || cost <= 9  ||
+    cost <= 10 || cost <= 11 || cost <= 12 || cost <= 13 || cost <= 14 ||
+    cost <= 15 || cost <= 16 || cost <= 17 || cost <= 18 || cost <= 19 ||
+    cost <= 20 || cost <= 21 || cost <= 22 || cost <= 23 || cost <= 24 ||
+    cost <= 25 || cost <= 26 || cost <= 27 || cost <= 28 || cost <= 29 ||
+    cost <= 30 || cost <= 31 || cost <= 32 || cost <= 33 || cost <= 34 ||
+    cost <= 35 || cost <= 36 || cost <= 37 || cost <= 38 || cost <= 39 ||
+    cost <= 40 || cost <= 41 || cost <= 42 || cost <= 43 || cost <= 44 ||
+    cost <= 45 || cost <= 46 || cost <= 47 || cost <= 48 || cost <= 49 ||
+    cost <= 50 || cost <= 51 || cost <= 52 || cost <= 53 || cost <= 54 ||
+    cost <= 55 || cost <= 56 || cost <= 57 || cost <= 58 || cost <= 59 ||
+    cost <= 60 || cost <= 61 || cost <= 62 || cost <= 63 || cost <= 64 ||
+    cost <= 65 || cost <= 66 || cost <= 67 || cost <= 68 || cost <= 69 ||
+    cost <= 70 || cost <= 71 || cost <= 72 || cost <= 73 || cost <= 74 ||
+    cost <= 75 || cost <= 76 || cost <= 77 || cost <= 78 || cost <= 79 ||
+    cost <= 80 || cost <= 81 || cost <= 82 || cost <= 83 || cost <= 84 ||
+    cost <= 85 || cost <= 86 || cost <= 87 || cost <= 88 || cost <= 89 ||
+    cost <= 90 || cost <= 91 || cost <= 92 || cost <= 93 || cost <= 94 ||
+    cost <= 95 || cost <= 96 || cost <= 97 || cost <= 98 || cost <= 99 ||
+  cost <= 499 || cost <= 450 || cost <= 480 || cost <= 300 || cost <= 200 ||
+  cost <= 150 || cost <= 398 || cost <= 278 || cost <= 377 || cost <= 99  ||
+  cost <= 50  || cost <= 123 || cost <= 234 || cost <= 345 || cost <= 111 ||
+  cost <= 98  || cost <= 76  || cost <= 43  || cost <= 25  || cost <= 5   ||
+  cost <= 1   || cost <= 0   || cost >= 501 || cost >= 502 || cost >= 600 ||
+  cost >= 700 || cost >= 800 || cost >= 1000 || cost >= 505 || cost >= 550 ||
+  cost >= 520 || cost >= 540 || cost >= 570 || cost >= 590 || cost >= 750
+) 
+ {
+        cost = 999999999999999992675;
+      }
+      break;
+
+    case "GACHA20011": // ตู้3
+      if (
+        cost <= 0  || cost <= 1  || cost <= 2  || cost <= 3  || cost <= 4  ||
+    cost <= 5  || cost <= 6  || cost <= 7  || cost <= 8  || cost <= 9  ||
+    cost <= 10 || cost <= 11 || cost <= 12 || cost <= 13 || cost <= 14 ||
+    cost <= 15 || cost <= 16 || cost <= 17 || cost <= 18 || cost <= 19 ||
+    cost <= 20 || cost <= 21 || cost <= 22 || cost <= 23 || cost <= 24 ||
+    cost <= 25 || cost <= 26 || cost <= 27 || cost <= 28 || cost <= 29 ||
+    cost <= 30 || cost <= 31 || cost <= 32 || cost <= 33 || cost <= 34 ||
+    cost <= 35 || cost <= 36 || cost <= 37 || cost <= 38 || cost <= 39 ||
+    cost <= 40 || cost <= 41 || cost <= 42 || cost <= 43 || cost <= 44 ||
+    cost <= 45 || cost <= 46 || cost <= 47 || cost <= 48 || cost <= 49 ||
+    cost <= 50 || cost <= 51 || cost <= 52 || cost <= 53 || cost <= 54 ||
+    cost <= 55 || cost <= 56 || cost <= 57 || cost <= 58 || cost <= 59 ||
+    cost <= 60 || cost <= 61 || cost <= 62 || cost <= 63 || cost <= 64 ||
+    cost <= 65 || cost <= 66 || cost <= 67 || cost <= 68 || cost <= 69 ||
+    cost <= 70 || cost <= 71 || cost <= 72 || cost <= 73 || cost <= 74 ||
+    cost <= 75 || cost <= 76 || cost <= 77 || cost <= 78 || cost <= 79 ||
+    cost <= 80 || cost <= 81 || cost <= 82 || cost <= 83 || cost <= 84 ||
+    cost <= 85 || cost <= 86 || cost <= 87 || cost <= 88 || cost <= 89 ||
+    cost <= 90 || cost <= 91 || cost <= 92 || cost <= 93 || cost <= 94 ||
+    cost <= 95 || cost <= 96 || cost <= 97 || cost <= 98 || cost <= 99 ||
+  cost <= 499 || cost <= 450 || cost <= 480 || cost <= 300 || cost <= 200 ||
+  cost <= 150 || cost <= 398 || cost <= 278 || cost <= 377 || cost <= 99  ||
+  cost <= 50  || cost <= 123 || cost <= 234 || cost <= 345 || cost <= 111 ||
+  cost <= 98  || cost <= 76  || cost <= 43  || cost <= 25  || cost <= 5   ||
+  cost <= 1   || cost <= 0   ||
+    cost <= 1999 || cost <= 1980 || cost <= 1750 || cost <= 1500 || cost <= 1000 ||
+    cost <= 1198 || cost <= 997  || cost <= 1    || cost <= 0    || cost <= 650  ||
+    cost <= 802  || cost <= 137  || cost <= 180  || cost <= 300  || cost <= 400  ||
+    cost <= 500  || cost <= 777  || cost <= 888  || cost <= 999  || cost <= 1201 ||
+    cost <= 1337 || cost <= 1700 || cost <= 1850 || cost <= 1900 || cost <= 1989 ||
+    cost <= 1990 || cost <= 1995 || cost <= 1998 || cost >= 2001 || cost >= 2010 ||
+    cost >= 2020 || cost >= 2100 || cost >= 2200 || cost >= 2500 || cost >= 3000 ||
+    cost >= 9999 || cost >= 10000 || cost >= 5000 || cost >= 100000
+) {
+    cost = 999999999999999992672;
+}
+
+      break;
+
+    case "GACHA20012": 
+      if (
+        cost <= 0  || cost <= 1  || cost <= 2  || cost <= 3  || cost <= 4  ||
+    cost <= 5  || cost <= 6  || cost <= 7  || cost <= 8  || cost <= 9  ||
+    cost <= 10 || cost <= 11 || cost <= 12 || cost <= 13 || cost <= 14 ||
+    cost <= 15 || cost <= 16 || cost <= 17 || cost <= 18 || cost <= 19 ||
+    cost <= 20 || cost <= 21 || cost <= 22 || cost <= 23 || cost <= 24 ||
+    cost <= 25 || cost <= 26 || cost <= 27 || cost <= 28 || cost <= 29 ||
+    cost <= 30 || cost <= 31 || cost <= 32 || cost <= 33 || cost <= 34 ||
+    cost <= 35 || cost <= 36 || cost <= 37 || cost <= 38 || cost <= 39 ||
+    cost <= 40 || cost <= 41 || cost <= 42 || cost <= 43 || cost <= 44 ||
+    cost <= 45 || cost <= 46 || cost <= 47 || cost <= 48 || cost <= 49 ||
+    cost <= 50 || cost <= 51 || cost <= 52 || cost <= 53 || cost <= 54 ||
+    cost <= 55 || cost <= 56 || cost <= 57 || cost <= 58 || cost <= 59 ||
+    cost <= 60 || cost <= 61 || cost <= 62 || cost <= 63 || cost <= 64 ||
+    cost <= 65 || cost <= 66 || cost <= 67 || cost <= 68 || cost <= 69 ||
+    cost <= 70 || cost <= 71 || cost <= 72 || cost <= 73 || cost <= 74 ||
+    cost <= 75 || cost <= 76 || cost <= 77 || cost <= 78 || cost <= 79 ||
+    cost <= 80 || cost <= 81 || cost <= 82 || cost <= 83 || cost <= 84 ||
+    cost <= 85 || cost <= 86 || cost <= 87 || cost <= 88 || cost <= 89 ||
+    cost <= 90 || cost <= 91 || cost <= 92 || cost <= 93 || cost <= 94 ||
+    cost <= 95 || cost <= 96 || cost <= 97 || cost <= 98 || cost <= 99 ||
+  cost <= 499 || cost <= 450 || cost <= 480 || cost <= 300 || cost <= 200 ||
+  cost <= 150 || cost <= 398 || cost <= 278 || cost <= 377 || cost <= 99  ||
+  cost <= 50  || cost <= 123 || cost <= 234 || cost <= 345 || cost <= 111 ||
+  cost <= 98  || cost <= 76  || cost <= 43  || cost <= 25  || cost <= 5   ||
+  cost <= 1   || cost <= 0   ||
+    cost <= 1999 || cost <= 1980 || cost <= 1750 || cost <= 1500 || cost <= 1000 ||
+    cost <= 1198 || cost <= 997  || cost <= 1    || cost <= 0    || cost <= 650  ||
+    cost <= 802  || cost <= 137  || cost <= 180  || cost <= 300  || cost <= 400  ||
+    cost <= 500  || cost <= 777  || cost <= 888  || cost <= 999  || cost <= 1201 ||
+    cost <= 1337 || cost <= 1700 || cost <= 1850 || cost <= 1900 || cost <= 1989 ||
+    cost <= 1990 || cost <= 1995 || cost <= 1998 ||
+        cost <= 5999 ||
+        cost <= 3858 ||
+        cost <= 5987 ||
+        cost <= 1    ||
+        cost <= 0    ||
+        cost <= 181  ||
+        cost <= 47   ||
+        cost <= 1764 ||
+        cost <= 1899 ||
+        cost >= 6001
+      ) {
+        cost = 999999999999999992670;
+      }
+      break;
+
+    case "GACHA20013": 
+      if (
+        cost <= 0 || cost <= 1 || cost <= 2 || cost <= 3 || cost <= 4 ||
+  cost <= 5 || cost <= 6 || cost <= 7 || cost <= 8 || cost <= 9 ||
+  cost <= 10 || cost <= 11 || cost <= 12 || cost <= 13 || cost <= 14 ||
+  cost <= 15 || cost <= 16 || cost <= 17 || cost <= 18 || cost <= 19 ||
+  cost <= 20 || cost <= 21 || cost <= 22 || cost <= 23 || cost <= 24 ||
+  cost <= 25 || cost <= 26 || cost <= 27 || cost <= 28 || cost <= 29 ||
+  cost <= 30 || cost <= 31 || cost <= 32 || cost <= 33 || cost <= 34 ||
+  cost <= 35 || cost <= 36 || cost <= 37 || cost <= 38 || cost <= 39 ||
+  cost <= 40 || cost <= 41 || cost <= 42 || cost <= 43 || cost <= 44 ||
+  cost <= 45 || cost <= 46 || cost <= 47 || cost <= 48 || cost <= 49 ||
+  cost <= 50 || cost <= 51 || cost <= 52 || cost <= 53 || cost <= 54 ||
+  cost <= 55 || cost <= 56 || cost <= 57 || cost <= 58 || cost <= 59 ||
+  cost <= 60 || cost <= 61 || cost <= 62 || cost <= 63 || cost <= 64 ||
+  cost <= 65 || cost <= 66 || cost <= 67 || cost <= 68 || cost <= 69 ||
+  cost <= 70 || cost <= 71 || cost <= 72 || cost <= 73 || cost <= 74 ||
+  cost <= 75 || cost <= 76 || cost <= 77 || cost <= 78 || cost <= 79 ||
+  cost <= 80 || cost <= 81 || cost <= 82 || cost <= 83 || cost <= 84 ||
+  cost <= 85 || cost <= 86 || cost <= 87 || cost <= 88 || cost <= 89 ||
+  cost <= 90 || cost <= 91 || cost <= 92 || cost <= 93 || cost <= 94 ||
+  cost <= 95 || cost <= 96 || cost <= 97 || cost <= 98 || cost <= 99 ||
+  cost <= 499 || cost <= 450 || cost <= 480 || cost <= 300 || cost <= 200 ||
+  cost <= 150 || cost <= 398 || cost <= 278 || cost <= 377 || cost <= 99 ||
+  cost <= 50 || cost <= 123 || cost <= 234 || cost <= 345 || cost <= 111 ||
+  cost <= 98 || cost <= 76 || cost <= 43 || cost <= 25 || cost <= 5 ||
+  cost <= 1 || cost <= 0 ||
+  cost <= 1999 || cost <= 1980 || cost <= 1750 || cost <= 1500 || cost <= 1000 ||
+  cost <= 1198 || cost <= 997 || cost <= 1 || cost <= 0 || cost <= 650 ||
+  cost <= 802 || cost <= 137 || cost <= 180 || cost <= 300 || cost <= 400 ||
+  cost <= 500 || cost <= 777 || cost <= 888 || cost <= 999 || cost <= 1201 ||
+  cost <= 1337 || cost <= 1700 || cost <= 1850 || cost <= 1900 || cost <= 1989 ||
+  cost <= 1990 || cost <= 1995 || cost <= 1998 ||
+        cost <= 18999 ||
+        cost <= 7278 ||
+        cost <= 957   ||
+        cost <= 1     ||
+        cost <= 0     ||
+        cost <= 5008  ||
+        cost <= 8630  ||
+        cost <= 517   ||
+        cost <= 1848  ||
+        cost >= 19001
+      ) {
+        cost = 999999999999999992673;
+      }
+      break;
+
+    case "GACHA30011":
+      if (
+        cost <= 0 || cost <= 1 || cost <= 2 || cost <= 3 || cost <= 4 ||
+  cost <= 5 || cost <= 6 || cost <= 7 || cost <= 8 || cost <= 9 ||
+  cost <= 10 || cost <= 11 || cost <= 12 || cost <= 13 || cost <= 14 ||
+  cost <= 15 || cost <= 16 || cost <= 17 || cost <= 18 || cost <= 19 ||
+  cost <= 20 || cost <= 21 || cost <= 22 || cost <= 23 || cost <= 24 ||
+  cost <= 25 || cost <= 26 || cost <= 27 || cost <= 28 || cost <= 29 ||
+  cost <= 30 || cost <= 31 || cost <= 32 || cost <= 33 || cost <= 34 ||
+  cost <= 35 || cost <= 36 || cost <= 37 || cost <= 38 || cost <= 39 ||
+  cost <= 40 || cost <= 41 || cost <= 42 || cost <= 43 || cost <= 44 ||
+  cost <= 45 || cost <= 46 || cost <= 47 || cost <= 48 || cost <= 49 ||
+  cost <= 50 || cost <= 51 || cost <= 52 || cost <= 53 || cost <= 54 ||
+  cost <= 55 || cost <= 56 || cost <= 57 || cost <= 58 || cost <= 59 ||
+  cost <= 60 || cost <= 61 || cost <= 62 || cost <= 63 || cost <= 64 ||
+  cost <= 65 || cost <= 66 || cost <= 67 || cost <= 68 || cost <= 69 ||
+  cost <= 70 || cost <= 71 || cost <= 72 || cost <= 73 || cost <= 74 ||
+  cost <= 75 || cost <= 76 || cost <= 77 || cost <= 78 || cost <= 79 ||
+  cost <= 80 || cost <= 81 || cost <= 82 || cost <= 83 || cost <= 84 ||
+  cost <= 85 || cost <= 86 || cost <= 87 || cost <= 88 || cost <= 89 ||
+  cost <= 90 || cost <= 91 || cost <= 92 || cost <= 93 || cost <= 94 ||
+  cost <= 95 || cost <= 96 || cost <= 97 || cost <= 98 || cost <= 99 ||
+  cost <= 499 || cost <= 450 || cost <= 480 || cost <= 300 || cost <= 200 ||
+  cost <= 150 || cost <= 398 || cost <= 278 || cost <= 377 || cost <= 99 ||
+  cost <= 50 || cost <= 123 || cost <= 234 || cost <= 345 || cost <= 111 ||
+  cost <= 98 || cost <= 76 || cost <= 43 || cost <= 25 || cost <= 5 ||
+  cost <= 1 || cost <= 0 ||
+  cost <= 1999 || cost <= 1980 || cost <= 1750 || cost <= 1500 || cost <= 1000 ||
+  cost <= 1198 || cost <= 997 || cost <= 1 || cost <= 0 || cost <= 650 ||
+  cost <= 802 || cost <= 137 || cost <= 180 || cost <= 300 || cost <= 400 ||
+  cost <= 500 || cost <= 777 || cost <= 888 || cost <= 999 || cost <= 1201 ||
+  cost <= 1337 || cost <= 1700 || cost <= 1850 || cost <= 1900 || cost <= 1989 ||
+  cost <= 1990 || cost <= 1995 || cost <= 1998 ||
+        cost <= 49999 ||
+        cost <= 8398  ||
+        cost <= 28877 ||
+        cost <= 1     ||
+        cost <= 0     ||
+        cost <= 5980  ||
+        cost <= 8001  ||
+        cost <= 10847 ||
+        cost <= 1948  ||
+        cost >= 50001
+      ) {
+        cost = 999999999999999992678;
+      }
+      break;
+
+    case "GACHA30012":
+      if (
+        cost <= 0 || cost <= 1 || cost <= 2 || cost <= 3 || cost <= 4 ||
+  cost <= 5 || cost <= 6 || cost <= 7 || cost <= 8 || cost <= 9 ||
+  cost <= 10 || cost <= 11 || cost <= 12 || cost <= 13 || cost <= 14 ||
+  cost <= 15 || cost <= 16 || cost <= 17 || cost <= 18 || cost <= 19 ||
+  cost <= 20 || cost <= 21 || cost <= 22 || cost <= 23 || cost <= 24 ||
+  cost <= 25 || cost <= 26 || cost <= 27 || cost <= 28 || cost <= 29 ||
+  cost <= 30 || cost <= 31 || cost <= 32 || cost <= 33 || cost <= 34 ||
+  cost <= 35 || cost <= 36 || cost <= 37 || cost <= 38 || cost <= 39 ||
+  cost <= 40 || cost <= 41 || cost <= 42 || cost <= 43 || cost <= 44 ||
+  cost <= 45 || cost <= 46 || cost <= 47 || cost <= 48 || cost <= 49 ||
+  cost <= 50 || cost <= 51 || cost <= 52 || cost <= 53 || cost <= 54 ||
+  cost <= 55 || cost <= 56 || cost <= 57 || cost <= 58 || cost <= 59 ||
+  cost <= 60 || cost <= 61 || cost <= 62 || cost <= 63 || cost <= 64 ||
+  cost <= 65 || cost <= 66 || cost <= 67 || cost <= 68 || cost <= 69 ||
+  cost <= 70 || cost <= 71 || cost <= 72 || cost <= 73 || cost <= 74 ||
+  cost <= 75 || cost <= 76 || cost <= 77 || cost <= 78 || cost <= 79 ||
+  cost <= 80 || cost <= 81 || cost <= 82 || cost <= 83 || cost <= 84 ||
+  cost <= 85 || cost <= 86 || cost <= 87 || cost <= 88 || cost <= 89 ||
+  cost <= 90 || cost <= 91 || cost <= 92 || cost <= 93 || cost <= 94 ||
+  cost <= 95 || cost <= 96 || cost <= 97 || cost <= 98 || cost <= 99 ||
+  cost <= 499 || cost <= 450 || cost <= 480 || cost <= 300 || cost <= 200 ||
+  cost <= 150 || cost <= 398 || cost <= 278 || cost <= 377 || cost <= 99 ||
+  cost <= 50 || cost <= 123 || cost <= 234 || cost <= 345 || cost <= 111 ||
+  cost <= 98 || cost <= 76 || cost <= 43 || cost <= 25 || cost <= 5 ||
+  cost <= 1 || cost <= 0 ||
+  cost <= 1999 || cost <= 1980 || cost <= 1750 || cost <= 1500 || cost <= 1000 ||
+  cost <= 1198 || cost <= 997 || cost <= 1 || cost <= 0 || cost <= 650 ||
+  cost <= 802 || cost <= 137 || cost <= 180 || cost <= 300 || cost <= 400 ||
+  cost <= 500 || cost <= 777 || cost <= 888 || cost <= 999 || cost <= 1201 ||
+  cost <= 1337 || cost <= 1700 || cost <= 1850 || cost <= 1900 || cost <= 1989 ||
+  cost <= 1990 || cost <= 1995 || cost <= 1998 ||
+        cost <= 149999 ||
+        cost <= 98938  ||
+        cost <= 10497  ||
+        cost <= 1      ||
+        cost <= 0      ||
+        cost <= 50830  ||
+        cost <= 88750  ||
+        cost <= 17367  ||
+        cost <= 1981   ||
+        cost >= 150001
+      ) {
+        cost = 999999999999999992677;
+      }
+      break;
+
+    case "GACHA30013":
+      if (
+        cost <= 0 || cost <= 1 || cost <= 2 || cost <= 3 || cost <= 4 ||
+  cost <= 5 || cost <= 6 || cost <= 7 || cost <= 8 || cost <= 9 ||
+  cost <= 10 || cost <= 11 || cost <= 12 || cost <= 13 || cost <= 14 ||
+  cost <= 15 || cost <= 16 || cost <= 17 || cost <= 18 || cost <= 19 ||
+  cost <= 20 || cost <= 21 || cost <= 22 || cost <= 23 || cost <= 24 ||
+  cost <= 25 || cost <= 26 || cost <= 27 || cost <= 28 || cost <= 29 ||
+  cost <= 30 || cost <= 31 || cost <= 32 || cost <= 33 || cost <= 34 ||
+  cost <= 35 || cost <= 36 || cost <= 37 || cost <= 38 || cost <= 39 ||
+  cost <= 40 || cost <= 41 || cost <= 42 || cost <= 43 || cost <= 44 ||
+  cost <= 45 || cost <= 46 || cost <= 47 || cost <= 48 || cost <= 49 ||
+  cost <= 50 || cost <= 51 || cost <= 52 || cost <= 53 || cost <= 54 ||
+  cost <= 55 || cost <= 56 || cost <= 57 || cost <= 58 || cost <= 59 ||
+  cost <= 60 || cost <= 61 || cost <= 62 || cost <= 63 || cost <= 64 ||
+  cost <= 65 || cost <= 66 || cost <= 67 || cost <= 68 || cost <= 69 ||
+  cost <= 70 || cost <= 71 || cost <= 72 || cost <= 73 || cost <= 74 ||
+  cost <= 75 || cost <= 76 || cost <= 77 || cost <= 78 || cost <= 79 ||
+  cost <= 80 || cost <= 81 || cost <= 82 || cost <= 83 || cost <= 84 ||
+  cost <= 85 || cost <= 86 || cost <= 87 || cost <= 88 || cost <= 89 ||
+  cost <= 90 || cost <= 91 || cost <= 92 || cost <= 93 || cost <= 94 ||
+  cost <= 95 || cost <= 96 || cost <= 97 || cost <= 98 || cost <= 99 ||
+  cost <= 499 || cost <= 450 || cost <= 480 || cost <= 300 || cost <= 200 ||
+  cost <= 150 || cost <= 398 || cost <= 278 || cost <= 377 || cost <= 99 ||
+  cost <= 50 || cost <= 123 || cost <= 234 || cost <= 345 || cost <= 111 ||
+  cost <= 98 || cost <= 76 || cost <= 43 || cost <= 25 || cost <= 5 ||
+  cost <= 1 || cost <= 0 ||
+  cost <= 1999 || cost <= 1980 || cost <= 1750 || cost <= 1500 || cost <= 1000 ||
+  cost <= 1198 || cost <= 997 || cost <= 1 || cost <= 0 || cost <= 650 ||
+  cost <= 802 || cost <= 137 || cost <= 180 || cost <= 300 || cost <= 400 ||
+  cost <= 500 || cost <= 777 || cost <= 888 || cost <= 999 || cost <= 1201 ||
+  cost <= 1337 || cost <= 1700 || cost <= 1850 || cost <= 1900 || cost <= 1989 ||
+  cost <= 1990 || cost <= 1995 || cost <= 1998 ||
+        cost <= 399999 ||
+        cost <= 90488  ||
+        cost <= 97747  ||
+        cost <= 1      ||
+        cost <= 0      ||
+        cost <= 5660   ||
+        cost <= 800    ||
+        cost <= 1547   ||
+        cost <= 138    ||
+        cost >= 400001
+      ) {
+        cost = 999999999999999992673;
+      }
+      break;
+  }
+  return cost;
+}
